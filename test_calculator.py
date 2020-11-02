89671215d3fee2da3ef0bcf4dc4a32759e7a1c86
Institute of Coding (Sunderland) <gavin.mcclary@sunderland.ac.uk>
2020-11-02 11:53:34 +0000

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -12,6 +12,3 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-
-    def test_multiplication(self):
-        assert 8 == calculator.multiply(4, 2)
